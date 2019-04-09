0ce218cfab2507f440833633387b963172c6ddd8
devingprice <devingprice@gmail.com>
2019-04-09 12:14:43 -0400

    Add favorites local storage and conditionals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 var topics = ['cat', 'dog']
 var activeTopic = null; 
 
+// Search Buttons
 function createButtons(items){
     for(var i=0;i< items.length; i++){
         var button = createButton(items[i])
@@ -13,68 +14,6 @@ function createButton(item){
 function renderButton(button){
     $('#buttonRow').append(button)
 }
-function requestGiphyImages(q, page){
-    console.log(page)
-    var giphyKey = 'qtUSsO1JbI1y0sSA1bhlWpmA29SW2dkr';
-    var url = '//api.giphy.com/v1/gifs/search?q='+q+
-    '&api_key='+giphyKey+'&limit=10&offset='+page;
-    return $.ajax({
-        url,
-        type : 'GET'
-    }).then(response => {
-        console.log(response)
-        for(var i = 0; i < response.data.length; i++){
-            renderImages(response.data[i], '#images')
-        }
-        addImageClick()
-        loadMoreButton(q)
-    })
-}
-function renderImages(imageData, destination){
-    var container = $('<div>');
-
-    $('<img>', {
-        class: 'giphyImg',
-        src: imageData.images.original_still.url,
-        'data-still': imageData.images.original_still.url,
-        'data-gif': imageData.images.original.url
-    }).appendTo(container)
-
-    createImageInfo(imageData).appendTo(container)
-
-    container.appendTo( destination )
-}
-function createImageInfo(imageData){
-    var rating = imageData.rating;
-    var title = imageData.title;
-    var score = imageData._score;
-    var time = imageData.trending_datetime;
-    var source = imageData.source_post_url;
-
-    var imageUrl = imageData.images.original.url;
-
-    return $('<div>', { class: 'img-info'}).append(
-        $('<h3>').text( title ),
-        $('<div>').text( 'rating: '+ rating ),
-        $('<div>').text( 'score: '+ score ),
-        $('<div>').text( 'time: '+ time ),
-        $('<a>').attr('href', source).text( 'source' ),
-        $('<a>').attr({
-            'target':"_blank",
-            'href':  imageUrl ,
-            'download':''
-        }).text('Download'),
-        addFavoritesButton(imageData)
-    )
-}
-
-function downloadClick(url, title){
-    window.downloads.download({
-        url,
-        filename: title
-    })
-}
-
 function addTopic(topicString){
     topics.push( topicString )
 
@@ -86,19 +25,6 @@ function addTopic(topicString){
     activeTopic = topicString;
     clearImages();
     requestGiphyImages( activeTopic, $('.giphyImg').length )
-}
-function addImageClick(){
-    $('.giphyImg').on('click', function(){
-        //toggle animate
-        console.log('click')
-        var url = $(this).attr('src');
-        var gif = $(this).attr('data-gif');
-        if( url === gif ){
-            $(this).attr('src', $(this).attr('data-still'))
-        } else {
-            $(this).attr('src', $(this).attr('data-gif'))
-        }
-    })
 }
 function addButtonClick(individual = null){
     if( individual ){
@@ -116,9 +42,89 @@ function buttonClick(){
     }
     requestGiphyImages( activeTopic, $('.giphyImg').length )
 }
-function clearImages(){
+function clearImages() {
     $('#images').empty()
 }
+
+
+// Loading Giphy Data
+function requestGiphyImages(q, page) {
+    console.log(page)
+    var giphyKey = 'qtUSsO1JbI1y0sSA1bhlWpmA29SW2dkr';
+    var url = '//api.giphy.com/v1/gifs/search?q=' + q +
+        '&api_key=' + giphyKey + '&limit=10&offset=' + page;
+    return $.ajax({
+        url,
+        type: 'GET'
+    }).then(response => {
+        console.log(response)
+        for (var i = 0; i < response.data.length; i++) {
+            renderImages(response.data[i], '#images')
+        }
+        addImageClick()
+        loadMoreButton(q)
+    })
+}
+
+// Image Card
+function addImageClick() {
+    $('.giphyImg').on('click', function () {
+        //toggle animate
+        console.log('click')
+        var url = $(this).attr('src');
+        var gif = $(this).attr('data-gif');
+        if (url === gif) {
+            $(this).attr('src', $(this).attr('data-still'))
+        } else {
+            $(this).attr('src', $(this).attr('data-gif'))
+        }
+    })
+}
+function renderImages(imageData, destination) {
+    var container = $('<div>', { class: 'imgCard' });
+
+    $('<img>', {
+        class: 'giphyImg',
+        src: imageData.images.original_still.url,
+        'data-still': imageData.images.original_still.url,
+        'data-gif': imageData.images.original.url
+    }).appendTo(container)
+
+    createImageInfo(imageData).appendTo(container)
+
+    container.appendTo(destination)
+}
+function createImageInfo(imageData) {
+    var rating = imageData.rating;
+    var title = imageData.title;
+    var score = imageData._score;
+    var time = imageData.trending_datetime;
+    var source = imageData.source_post_url;
+
+    var imageUrl = imageData.images.original.url;
+
+    return $('<div>', { class: 'img-info' }).append(
+        $('<h3>').text(title),
+        $('<div>').text('rating: ' + rating),
+        $('<div>').text('score: ' + score),
+        $('<div>').text('time: ' + time),
+        $('<a>').attr('href', source).text('source'),
+        $('<a>').attr({
+            'target': "_blank",
+            'href': imageUrl,
+            'download': ''
+        }).text('Download'),
+        addFavoritesButton(imageData)
+    )
+}
+function downloadClick(url, title) {
+    window.downloads.download({
+        url,
+        filename: title
+    })
+}
+
+// Load More Button
 function loadMoreButton(q = null){
     $('#loadMore').show()
     if( q ){
@@ -130,46 +136,83 @@ function hideLoadMore(){
     $('#loadMore').hide();
 }
 
-var favorites = {'favorites': []};
-function storeFavorites(item){
-    var currentFavorites = favorites;//retrieveFavorites();
-    currentFavorites.favorites.push(item);
-    console.log(favorites)
-    localStorage.setItem('favorites', currentFavorites);
-}
-function retrieveFavorites(){
-    return localStorage.getItem('favorites');
-}
-function clearFavorites(){
-    localStorage.clear();
-}
-
-
-function addFavoritesButton(imageData){
-    return $('<button>').text('Fav').on('click', function(){
-        storeFavorites(imageData)
-    })
-}
-function clickFavoritesTab(){
+//TABS
+function clickFavoritesTab() {
     console.log('clicked fav')
     $('#images').removeClass('active')
-    $('#images').addClass('hidden')
+    $('#images').addClass('hidden') //images has display flex set by id so it has to be overwritten with display:none!important
     $('#favorites').addClass('active')
+    $('#buttonRow').hide();
 
     $('#favorites').empty();
-    for( var i = 0; i < favorites.favorites.length;i++){
-        renderImages( favorites.favorites[i], "#favorites")
+    $('#favorites').append($('<button>').text('Clear').on('click', function () {
+        clearFavorites()
+    }));
+    var favorites = retrieveFavorites();
+    for (var i = 0; i < favorites.length; i++) {
+        renderImages(favorites[i], "#favorites")
     }
     addImageClick();
     hideLoadMore();
 }
-function clickSearchTab(){
+function clickSearchTab() {
     console.log('clicked search')
     $('#favorites').removeClass('active')
     $('#images').addClass('active')
     $('#images').removeClass('hidden')
+    $('#buttonRow').show();
+
     loadMoreButton();
 }
+
+// FAVORITES
+function storeFavorites(item){
+    var currentFavorites = retrieveFavorites();
+    console.log( currentFavorites)
+    currentFavorites.push(item);
+    localStorage.setItem('favorites', JSON.stringify(currentFavorites));
+}
+function retrieveFavorites(){
+    var favorites = [];
+    var retrievedFavorites = JSON.parse(localStorage.getItem('favorites'));
+    if (retrievedFavorites){
+        favorites = retrievedFavorites;
+    }
+    return favorites;
+}
+function clearFavorites(){
+    localStorage.clear();
+    $('#favorites').empty();
+    $('.favButton').html('<i class="far fa-heart"></i>')
+}
+function addFavoritesButton(imageData){
+    var favButton =  $('<div>', {class: 'favButton'})
+        .html('<i class="far fa-heart"></i>')
+        .on('click', function(){
+            if( !containedInFavorites(imageData.url) ){
+                storeFavorites(imageData)
+                $(this).html('<i class="fas fa-heart"></i>')
+            }
+            
+        });
+    if( containedInFavorites(imageData.url) ){
+        favButton.html('<i class="fas fa-heart"></i>')
+    }
+    return favButton;
+}
+function containedInFavorites(url){
+    var favorites = retrieveFavorites();
+    
+    for(var i=0; i< favorites.length; i++){
+        if( favorites[i].url === url){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+// STARTUP 
 
 function initialize(){
     createButtons(topics);
